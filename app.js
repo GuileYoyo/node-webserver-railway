e41b0e390e03b55f845e3fffa740b8c1856a4f34
Guillermo Garcia <guillermoyoyo@gmail.com>
2023-10-09 18:30:09 +0200

    React app ready

--- app.js
+++ app.js
@@ -1,44 +1,42 @@
-const express = require('express');
-const hbs = require('hbs');
-require('dotenv').config();
+const express = require("express");
+const hbs = require("hbs");
+require("dotenv").config();
 
-
-const app = express()
+const app = express();
 const port = process.env.PORT;
 
 // Handlebars
-app.set('view engine', 'hbs');
-hbs.registerPartials(__dirname + '/views/partials')
-
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
 
 // Servir contenido estatico
-app.use( express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.render('home', {
-        nombre: 'Fernando Herrera',
-        titulo: 'Curso de Node'
-    });
-})
+app.get("/", (req, res) => {
+  res.render("home", {
+    nombre: "Fernando Herrera",
+    titulo: "Curso de Node",
+  });
+});
 
-app.get('/generic', (req, res) => {
-    res.render('generic', {
-        nombre: 'Fernando Herrera',
-        titulo: 'Curso de Node'
-    });
-})
+app.get("/generic", (req, res) => {
+  res.render("generic", {
+    nombre: "Fernando Herrera",
+    titulo: "Curso de Node",
+  });
+});
 
-app.get('/elements', (req, res) => {
-    res.render('elements', {
-        nombre: 'Fernando Herrera',
-        titulo: 'Curso de Node'
-    });
-})
+app.get("/elements", (req, res) => {
+  res.render("elements", {
+    nombre: "Fernando Herrera",
+    titulo: "Curso de Node",
+  });
+});
 
-app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/public/404.html');
-})
+// app.get('*', (req, res) => {
+//     res.sendFile(__dirname + '/public/404.html');
+// })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
